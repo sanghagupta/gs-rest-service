7ddd260bb1faf10b0f9c812f91893031c1b9f7cb
sanghagupta <44203967+sanghagupta@users.noreply.github.com>
2018-10-16 20:37:07 +0530

    Update WordController.java

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -2,7 +2,71 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 
+class Pallindrom{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+    
+    @RequestMapping(value="/words/{word}", method = RequestMethod.GET)
+    public Pallindrom testPallin(
+            @PathVariable("word") String word){
+     
+        
+        String  reverse = ""; // Objects of String class
+        boolean pallindrom=false, anagramOfPallindrom=false;
+     
+      int length = word.length();
+     
+      for (int i = length - 1; i >= 0; i--)
+         reverse = reverse + word.charAt(i);
+         
+      if (word.equals(reverse)){
+         pallindrom=true; 
+          anagramOfPallindrom=true;
+      }
+      
+        
+        if(pallindrom==false){
+        
+            final int NO_OF_CHARS = 256; 
+              // Create a count array and initialize 
+             // all values as 0 
+             int[] count = new int[NO_OF_CHARS]; 
+  
+        // For each character in input strings, 
+        // increment count in the corresponding 
+        // count array 
+        for (int i = 0; i < word.length(); i++) 
+            count[word.charAt(i)]++; 
+  
+        // Count odd occurring characters 
+        int odd = 0; 
+        for (int i = 0; i < NO_OF_CHARS; i++) { 
+            if ((count[i] & 1) != 0) 
+                odd++; 
+  
+            if (odd > 1) 
+                anagramOfPallindrom= false; 
+        } 
+  
+        // anagramOfPallindrom is true if odd count is 0 or 1, 
+        anagramOfPallindrom= true; 
+            
+        }
+        
+        
+        
+      
+    }
 }
