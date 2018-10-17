69bd850502978d72a463c1dea07a4f96d9738476
sanghagupta <44203967+sanghagupta@users.noreply.github.com>
2018-10-17 10:28:36 +0530

    Update Pallindrome.java

--- src/main/java/hello/Pallindrome.java
+++ src/main/java/hello/Pallindrome.java
@@ -4,13 +4,17 @@ package hello;
 public class Pallindrome {
 
 	private String word;
-	private boolean pallindrom;
-	private boolean anagramOfPallindrom;
+	private boolean palindrome;
+	private boolean anagramOfPalindrome;
 	
 	
+
+
 	public String getWord() {
 		return word;
 	}
+
+
 
 
 	public void setWord(String word) {
@@ -18,31 +22,41 @@ public class Pallindrome {
 	}
 
 
-	public boolean isPallindrom() {
-		return pallindrom;
+
+
+	public boolean isPalindrome() {
+		return palindrome;
 	}
 
 
-	public void setPallindrom(boolean pallindrom) {
-		this.pallindrom = pallindrom;
+
+
+	public void setPalindrome(boolean palindrome) {
+		this.palindrome = palindrome;
 	}
 
 
-	public boolean isAnagramOfPallindrom() {
-		return anagramOfPallindrom;
+
+
+	public boolean isAnagramOfPalindrome() {
+		return anagramOfPalindrome;
 	}
 
 
-	public void setAnagramOfPallindrom(boolean anagramOfPallindrom) {
-		this.anagramOfPallindrom = anagramOfPallindrom;
+
+
+	public void setAnagramOfPalindrome(boolean anagramOfPalindrome) {
+		this.anagramOfPalindrome = anagramOfPalindrome;
 	}
 
 
-	public Pallindrome(String word, boolean pallindrom, boolean anagramOfPallindrom) {
+
+
+	public Pallindrome(String word, boolean pallindrom, boolean anagramOfPalindrome) {
 		// TODO Auto-generated constructor stub
 		this.word=word;
-		this.pallindrom=pallindrom;
-		this.anagramOfPallindrom=anagramOfPallindrom;
+		this.palindrome=pallindrom;
+		this.anagramOfPalindrome=anagramOfPalindrome;
 	}
 
 }
